08954e2482887495f906f3a5b360aaf1ce87f88e
jacobperry <perryjacob@outlook.com>
2023-10-25 23:54:52 -0700

    Prep 3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random rngGenerator = new Random();
+        int randomNumber = rngGenerator.Next(1, 100);
+
+        int maxAttempts = 10;
+        int attempts = 0;
+
+        bool isGuessed = false;
+
+        while(attempts < maxAttempts && !isGuessed) {
+            Console.Write("What is your guess? (1-100) ");
+            int guess = int.Parse(Console.ReadLine());
+
+            if(guess == randomNumber) {
+                Console.WriteLine($"Congrats! You guessed the correct number in {attempts} attempts.");
+                break;
+            }
+
+            Console.Clear();
+            Console.WriteLine("==============================");
+            Console.Write($"{guess} was incorrect. ");
+            if(guess < randomNumber) {
+                Console.WriteLine("The number is higher.");
+            } else {
+                Console.WriteLine("The number is lower.");
+            }
+            Console.WriteLine("==============================");
+
+            attempts++;
+        }
     }
 }
