0a8540160c47aab809a4f2c3432deaa3de3e7e47
jacobperry <perryjacob@outlook.com>
2023-11-09 07:01:11 -0800

    fractions

--- prepare/Learning03/Program.cs
+++ prepare/Learning03/Program.cs
@@ -4,6 +4,20 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning03 World!");
+        Fraction defaultFraction = new Fraction();
+        Console.WriteLine(defaultFraction.GetFractionString());
+        Console.WriteLine(defaultFraction.GetDecimalValue());
+
+        Fraction wholeNumberFraction = new Fraction(5);
+        Console.WriteLine(wholeNumberFraction.GetFractionString());
+        Console.WriteLine(wholeNumberFraction.GetDecimalValue());
+
+        Fraction threeQuarter = new Fraction(3, 4);
+        Console.WriteLine(threeQuarter.GetFractionString());
+        Console.WriteLine(threeQuarter.GetDecimalValue());
+
+        Fraction third = new Fraction(1, 3);
+        Console.WriteLine(third.GetFractionString());
+        Console.WriteLine(third.GetDecimalValue());
     }
 }
