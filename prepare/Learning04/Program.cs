851dba4bb9623a3e0749bd4f0912f2396f570287
Jacob <perryjacob@outlook.com>
2023-11-21 10:53:18 -0800

    Learning w04 - Inheritance

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning04 World!");
+        MathAssignment assignment1 = new MathAssignment("Jacob", "Fractions", "1.5", "2-4, 6, 9-11");
+
+        Console.Clear();
+        Console.WriteLine(assignment1.GetSummary());
+        Console.WriteLine(assignment1.GetHomeworkList());
+
+        Console.WriteLine();
+
+        WritingAssignment assignment2 = new WritingAssignment("Jacob", "Creative Writing", "A Day on Mars");
+
+        Console.WriteLine(assignment2.GetSummary());
+        Console.WriteLine(assignment2.GetWritingInformation());
     }
 }
