e65068883a15b25c660227ec20ac70531e658003
jacobperry <perryjacob@outlook.com>
2023-11-28 16:04:34 -0800

    W05 Polymorphism

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s = new Square("Midori", 10);
+        shapes.Add(s);
+        Rectangle r = new Rectangle("Ao", 5, 12);
+        shapes.Add(r);
+        Circle c = new Circle("Aka", 4);
+        shapes.Add(c);
+
+        foreach (Shape shape in shapes)
+        {
+            string color = shape.GetColor();
+            double area = shape.GetArea();
+
+            Console.WriteLine($"This shape is {color} with an area of {area}");
+        }
     }
 }
